4c0988b5e2f3982a0736da5ab9fec7d429c65407
jeff <jeve0658@colorado.edu>
2019-01-29 18:26:58 -0700

    goal service

--- SDPP_gazebo/src/model_walk.cpp
+++ SDPP_gazebo/src/model_walk.cpp
@@ -4,7 +4,14 @@
 #include <gazebo/common/common.hh>
 #include <ignition/math/Vector3.hh>
 
+#include <thread>
 #include "ros/ros.h"
+#include "ros/callback_queue.h"
+#include "ros/subscribe_options.h"
+#include "std_msgs/Float32.h"
+#include "geometry_msgs/Pose.h"
+#include "sdpp_gazebo/set_goal.h"
+
 
 #include <iostream>
 
@@ -21,6 +28,37 @@ namespace gazebo
 			// simulation iteration/
 			this->updateConnection = event::Events::ConnectWorldUpdateBegin(
 					std::bind(&ModelWalk::OnUpdate, this));
+
+
+
+			if (!ros::isInitialized())
+			{
+			  int argc = 0;
+			  char **argv = NULL;
+			  ros::init(argc, argv, "gazebo_walker_node",
+			      ros::init_options::NoSigintHandler);
+			}
+
+			// Create our ROS node. This acts in a similar manner to
+			// the Gazebo node
+			this->rosNode.reset(new ros::NodeHandle("gazebo_walker_node"));
+
+
+			this->rosServ = n.advertiseService("test", &ModelWalk::onService);
+			/*
+			// Create a named topic, and subscribe to it.
+			ros::SubscribeOptions so =
+				ros::SubscribeOptions::create<std_msgs::Float32>(
+			    "/" + this->model->GetName() + "/walk_goal",
+				1,
+			    boost::bind(&ModelWalk::OnRosMsg, this, _1),
+			    ros::VoidPtr(), &this->rosQueue);
+
+			this->rosSub = this->rosNode->subscribe(so);
+
+			// Spin up the queue helper thread.
+			this->rosQueueThread =
+			  std::thread(std::bind(&ModelWalk::QueueThread, this));*/
 		}
 
 		public: void OnUpdate()
@@ -39,9 +77,30 @@ namespace gazebo
 			}
 
 			math::Box bounding = this->model->GetBoundingBox();
-			std::cout << bounding << std::endl;
-
+			//std::cout << bounding << std::endl;
 		}
+
+		public: void OnRosMsg(const std_msgs::Float32ConstPtr &_msg)
+		{
+		  std::cout << _msg->data << std::endl;
+		}
+
+		public: void onService(sdpp_gazebo::set_goal::Request &req,
+							   sdpp_gazebo::set_goal::Request &req){
+
+			res.plan_success = "test";
+		}
+
+		private: void QueueThread()
+		{
+		  static const double timeout = 0.01;
+		  while (this->rosNode->ok())
+		  {
+		    this->rosQueue.callAvailable(ros::WallDuration(timeout));
+		  }
+		}
+
+
 
 		// POinter to the model
 		private: physics::ModelPtr model;
@@ -50,8 +109,22 @@ namespace gazebo
 		private: event::ConnectionPtr updateConnection;
 
 		// desired goal pose
-		public: math::Pose goal_pose;
+		public: std_msgs::Float32 goal_pose;
+		
+		/// \brief A node use for ROS transport
+		private: std::unique_ptr<ros::NodeHandle> rosNode;
 
+		//a ros service brief
+		private: ros::ServiceServer rosServ;
+
+		/// \brief A ROS subscriber
+		private: ros::Subscriber rosSub;
+
+		/// \brief A ROS callbackqueue that helps process messages
+		private: ros::CallbackQueue rosQueue;
+
+		/// \brief A thread the keeps running the rosQueue
+		private: std::thread rosQueueThread;
 
 	};
 	GZ_REGISTER_MODEL_PLUGIN(ModelWalk)

ff6a30f810b141cf0911a99eee0dced0916dd051
belgiumkansas <belgiumkansas@gmail.com>
2019-02-11 14:20:36 -0700

    works enough for some data

--- SDPP_gazebo/scripts/map_script.py
+++ SDPP_gazebo/scripts/map_script.py
@@ -8,7 +8,9 @@ from geometry_msgs.msg import Pose
 from gazebo_msgs.srv import GetModelState
 from gazebo_msgs.msg import ModelState
 
-depth = 3
+import matplotlib.pyplot as plt
+
+
 
 class ConfigSpaceGraph(object):
 
@@ -26,71 +28,76 @@ class ConfigSpaceGraph(object):
 		return 
 
 
-
 class HeadingGraph(object):
-
 	def __init__(self, map_meta_data):
+		"""
+		create the basic heading graph necessities
+		:param map_meta_data: meta data of gra
+		"""
 
 		self.MapMetaData = map_meta_data
 		self.GoalCoordinate = [0]*2
 
-		x_cells = map_meta_data.width
-		y_cells = map_meta_data.height
+		# set array around origin of zero
+		x_width = map_meta_data.width*map_meta_data.resolution
+		x_max = x_width + map_meta_data.origin.position.x
+		x_min = x_max - x_width
 
-		self.heading_graph = np.zeros((x_cells , y_cells))
+		y_height = map_meta_data.height*map_meta_data.resolution
+		y_max = y_height + map_meta_data.origin.position.y
+		y_min = y_max - y_height
 
-	def _array_cord_tf(self, m, n):
+		# create the mesh array to align the
+		self.xx, self.yy = np.meshgrid(	np.arange(x_min, x_max, map_meta_data.resolution),
+										np.arange(y_min, y_max, map_meta_data.resolution),
+										sparse=True)
+
+		# create heading array
+		self.heading_array = np.zeros((self.xx.size, self.yy.size))
+
+	def _array_to_coord(self, i, j):
 		"""
 		take a array index and transform to coordinate location
-		:param m: index location
-		:param n: index location
-		:return: coordinate list
+		:param i: index location
+		:param j: index location
+		:return: coordinate [x, y]
 		"""
-		x = n - self.MapMetaData.origin.position.x
-		y = m - self.MapMetaData.origin.position.y
+		x = self.xx[0][j]
+		y = self.yy[i][0]
 
 		return [x, y]
 
-	def _cord_array_tf(self,x, y):
+	def _coord_to_array(self,x, y):
 		"""
-		take coordinate location and transform to array index
+		take coordinate location and transform to nearest array index
 		:param x: coordinate location
 		:param y: coordinate location
-		:return: index list
+		:return: index [i, j]
 		"""
-		m = y + self.MapMetaData.origin.position.y
-		n = x + self.MapMetaData.origin.position.x
-		return m, n
+		idj = (np.abs(self.xx[0] - x).argmin())
+		idi = (np.abs(self.yy - y).argmin())
 
-	def get_heading_from_coordinate(self, x, y):
-
-		m , n = self._cord_array_tf(x, y)
-
-		return self.heading_graph[m, n]
-
-	def get_x_y_magnitude(self, x, y):
-
-		theta = self.get_heading_from_coordinate(x, y)
-
-		x = math.cos(theta)
-		y = math.sin(theta)
-
-		return x, y
-
-
+		return [idi, idj]
 
 	def build_heading_graph(self, goal_point):
 
 		self.GoalCoordinate = goal_point
 
-		for index, data in np.ndenumerate(self.heading_graph):
-			x, y = self._array_cord_tf(index[0], index[1])
-
+		for index, data in np.ndenumerate(self.heading_array):
+			x, y = self._array_to_coord(index[0], index[1])
 			x = self.GoalCoordinate[0] - x
 			y = self.GoalCoordinate[1] - y
+			self.heading_array[index] = math.atan2(y, x)
 
-			self.heading_graph[index] = math.atan2(y, x)
+	def get_heading_components(self, x, y):
 
+		idi, idj = self._coord_to_array(x, y)
+
+		m = self.heading_array[idi][idj]
+		xh = math.cos(m)
+		yh = math.sin(m)
+
+		return xh, yh
 
 
 
@@ -104,21 +111,19 @@ if __name__ == '__main__':
 	person_1_service = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
 
 	MapMeta = MapMetaData()
-	MapMeta.resolution = 1
-	MapMeta.width = 10
-	MapMeta.height = 10
-	MapMeta.origin.position.x = 2
-	MapMeta.origin.position.y = 2
+	MapMeta.resolution = .1
+	MapMeta.width = 200				#in cells
+	MapMeta.height = 200 			#in cells
+	MapMeta.origin.position.x = -10 #in what?
+	MapMeta.origin.position.y = -10 #in what?
 	MapMeta.origin.position.z = 0
 
-	goal_one = HeadingGraph(MapMeta)
+	goal_one_heading = HeadingGraph(MapMeta)
 
-	goal_one.build_heading_graph((1, 1))
-
-
-	print goal_one.get_heading_from_coordinate(-2 , 2)
+	goal_one_heading.build_heading_graph((3, 4))
 
 	rate = rospy.Rate(10)
+
 	try:
 		#publish map
 		while not rospy.is_shutdown():
@@ -130,20 +135,16 @@ if __name__ == '__main__':
 
 			pose = Pose()
 
-			xv, yv = goal_one.get_x_y_magnitude(x, y)
+			xh, yh = goal_one_heading.get_heading_components(x, y)
 
-			pose.position.x = xv*.2
-			pose.position.y = yv*.2
+			pose.position.x = xh*.4
+			pose.position.y = yh*.4
 			person_1_pub.publish(pose)
 			rate.sleep()
-
-
 
 	except rospy.ROSInterruptException:
 		pass
 
 	print "end of trial"
-
-
 
 

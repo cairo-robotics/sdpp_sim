76925419ff088a4163a6b77f5798f2de85df7cbf
belgiumkansas <jeve0658@colorado.edu>
2019-10-15 20:48:13 -0600

    map load class built

--- sdpp_navigation/scripts/load_map.py
+++ sdpp_navigation/scripts/load_map.py
@@ -1,64 +1,13 @@
 #!/usr/bin/env python
 
-from sdpp_navigation.grid_world import GridWorld, ValueIterationAlgo
-import matplotlib.pyplot as plt
-from pprint import pprint, pformat
 import rospy
-from nav_msgs.msg import OccupancyGrid
-from nav_msgs.srv import GetMap
-
-from PIL import Image
 import numpy as np
 
+import matplotlib.pyplot as plt
 
-class LoadMap(object):
-    def __init__(self, file_location=None):
-
-        self.static_odom_map = None
-
-        rospy.loginfo(rospy.get_name() + ": getting map from map server")
-
-        if file_location is None:
-            self.static_odom_map = self.load_from_static_map_srv()
-        else:
-            self.static_odom_map = self.load_from_file_location_map(file_location)
-
-    @staticmethod
-    def load_from_static_map_srv(srv_name = '/static_map'):
-        rospy.wait_for_service(srv_name)
-        static_map_srv = rospy.ServiceProxy(srv_name, GetMap)
-
-        try:
-            static_odom_msg = static_map_srv().map
-            rospy.loginfo(rospy.get_name() + ": received static map")
-
-        except rospy.ServiceException as exc:
-            rospy.logerror(rospy.get_name() + ": unable to get static map" + str(exc))
-            return None
-
-        return static_odom_msg
-
-    @staticmethod
-    def load_from_file_location_map(file_location):
-
-        rospy.logerror(rospy.get_name() + ": load map directly not finished")
-        im_frame = Image.open('block_room.png')
-        np_frame = np.array(im_frame)
-        np_frame = np_frame[:, :, 0]
-        np_frame = np.rot90(np_frame, 3)
-
-        return np_frame
-
-    def gridworld_format_data(self):
-
-        width = self.static_odom_map.info.width
-        height = self.static_odom_map.info.height
-
-        numpy_array = np.asarray(self.static_odom_map.data)
-        numpy_array = numpy_array.reshape(width, height)
-
-        kwags = {"array": numpy_array, "world_bounds_rows": width, "world_bounds_cols": height}
-        return kwags
+from nav_msgs.msg import OccupancyGrid
+from sdpp_navigation.grid_world import GridWorld, ValueIterationAlgo
+from sdpp_navigation.ros_wrappers import LoadMap
 
 
 

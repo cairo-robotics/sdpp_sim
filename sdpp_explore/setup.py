a114cac9ce3e91eba2f1d55f2652f9bf12955252
jeff <jeve0658@colorado.edu>
2019-12-11 19:22:52 -0700

    move base files and classes for explore

--- sdpp_explore/setup.py
+++ sdpp_explore/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['sdpp_exploit'],
+    packages=['sdpp_explore'],
     package_dir={'': 'src'},
 )
 

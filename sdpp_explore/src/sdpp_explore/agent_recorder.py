15ac16cbc3ec2a1e590e2a0d5f2da2cb3a0514a1
jeff <jeve0658@colorado.edu>
2019-12-16 21:01:41 -0700

    agent recorder object state machine

--- sdpp_explore/src/sdpp_explore/agent_recorder.py
+++ sdpp_explore/src/sdpp_explore/agent_recorder.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import rospy
+import os
 
 import pandas as pd
 import numpy as np
@@ -15,35 +16,87 @@ import matplotlib.pyplot as plt
 
 
 class AgentRecorder(object):
-    def __init__(self):
-        pass
+    def __init__(self, **configs):
 
-    def _init_dict(self):
-        pass
+        self.agent_builders = {'gazebo': AgentTrajGazebo}
+        self.agent_traj_gazebo_config = {}
+        self.timer_callback_delay = 5
+        
+        self.__dict__.update(configs["AgentRecorder"])
 
-    def record_callback(self):
-        pass
-    
-    def record_local(self):
-        pass
+        self.agent_dict = {}
+        #start factory and add register builders 
+        self.agent_factory = AgentTrajFactory()
+        for key in self.agent_builders:
+            builder = self.agent_builders[key]
+            self.agent_factory.register_builder(key, builder)
+        
+        
+        self.add_new_agents()
+        #self.timed = rospy.Timer(rospy.Duration(self.timer_callback_delay), self.add_new_agents())
+            
+    def add_new_agents(self):
+        
+        new_agents = self.scan_odom()
+        for agent_tuple in new_agents:
+            print(agent_tuple)
 
-    def record_file(self):
-        pass
-    
+            agent_object = self.agent_factory.create(agent_tuple[1] )
+            self.agent_dict[agent_tuple[0]] = agent_object
+
+
+    def scan_odom(self):
+        """
+        scans all the topics for odometry and returns namespace
+        of valid new agents
+        return: (agent_namespace, agent_node_origin)
+        """
+        published_topics = rospy.get_published_topics()
+
+        # check for all topics with Odometry message type
+        odom_proposals = []
+        for topic in published_topics:
+            if topic[1] == "nav_msgs/Odometry":
+                odom_proposals.append(topic[0])
+        
+        #filer out truth data odom
+        #find namespace and node origin
+        new_agents = []
+        for odom_topic in odom_proposals:
+            if "odom" in odom_topic:
+                agent_ns = odom_topic.replace('/odom', '')
+                if agent_ns not in self.agent_dict:
+                    agent_node = self._check_topic_node_origin(odom_topic)
+                    new_agents.append((agent_ns, agent_node))
+                    rospy.loginfo("agent {} of {} node found".format(agent_ns, agent_node))
+                else:
+                    rospy.loginfo("agent {} already built".format(agent_ns))
+            else:
+               pass
+        return new_agents
+
+    def _check_topic_node_origin(self, topic):
+        """
+        checks topic to find originating node for 
+
+        """
+        cmd = "rostopic info /human_0/odom"
+        unused_currently = os.system(cmd)
+        return "gazebo"
 
 
 class AgentTrajFactory(object):
     """
     creates AgentTrajectory objects
+    includes service builder registration
+    for dynamic loading of ROS traj nodes
     """
-
     def __init__(self):
         self._builder = {}
         reg_srv = rospy.Service('agent_traj_register', 
                                 AgentTrajRegister,  
                                 self.register_builder_server)
     
-
     def register_builder(self, key, builder):
         self._builder[key] = builder
         rospy.loginfo("registered {} builder".format(builder.__name__))
@@ -63,7 +116,8 @@ class AgentTrajFactory(object):
 
         else:
             return AgentTrajRegister(False, "not valid Agent Trag {} ".format(req))
-
+    
+    
 
 class AgentTrajGazebo(object):
     def __init__(self, **config):
@@ -79,7 +133,6 @@ class AgentTrajGazebo(object):
 
         sub_topic = self.agent_ns + "/odom"
         rospy.Subscriber(sub_topic, Odometry, self._traj_callback)
-
 
     def set_recording(self, bool_record):
         
